80b78a14e3f4eb670b024057ada11047b7f0a7cb
ADJ1ZGR8 <156932616+ADJ1ZGR8@users.noreply.github.com>
2024-03-22 13:38:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 
-// 
-
-
-
-
-
 document.addEventListener('keydown', (event) => {
-    const key = event.key; 
-    
+    const key = event.key;
     const display = document.querySelector('input[name="display"]');
 
+    
     if (event.code.startsWith('F')) {
         event.preventDefault(); 
-        return;
+        return; 
     }
 
+   
     if (/[0-9]/.test(key)) {
-        display.value += key; 
+        display.value += key;
     }
 
     
     if (['+', '-', '*', '/'].includes(key)) {
-        display.value += key; 
-    }
-
-   
-    if (key === '.' && !display.value.includes('.')) {
-        display.value += key; 
+        display.value += key;
     }
 
     
+    if (key === '.' && !display.value.includes('.')) {
+        display.value += key;
+    }
+
+   
     if (key === 'Enter') {
-        display.value = eval(display.value); 
+        display.value = eval(display.value);
     }
 
     
     if (key === 'Backspace') {
-        display.value = display.value.slice(0, -1);     }
-
-    
-    if (key === 'Delete') {
-        display.value = ''; 
+        display.value = display.value.slice(0, -1);
     }
 
-    
    
+    if (key === 'Delete') {
+        display.value = '';
+    }
+
 });
